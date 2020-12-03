8f7f9ffc2b3158f5f070849668630d2419f47068
unknown <deveshk.ryan@gmail.com>
2020-12-03 20:21:40 +0530

    style debugging

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
 import { Loading, Header, Card } from "../components";
+import { FooterContainer } from "./footer";
 import * as ROUTES from "../constants/routes";
 import logo from "../logo.svg";
 
@@ -106,6 +107,7 @@ export const BrowseContainer = ({ slides }) => {
           </Card>
         ))}
       </Card.Group>
+      <FooterContainer />
     </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
